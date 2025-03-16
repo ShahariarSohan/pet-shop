4c1d25f8d5633410d14db8a8d4ba02c70a4a0068
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-03-16 15:13:33 +0600

    all category show functionality added

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 // display category button
-const displayCategory = async () => {
+const displayCategoryButton = async () => {
     const res = await fetch("https://openapi.programming-hero.com/api/peddy/categories")
     const data = await res.json();
     const categoryData = data.categories;
@@ -15,4 +15,39 @@ const displayCategory = async () => {
         categoryContainer.append(div)
     });
 }
-displayCategory()
+displayCategoryButton()
+
+// display all category
+const displayAllCategories = async () => {
+    const cardContainer = document.getElementById('card-container')
+    const res = await fetch("https://openapi.programming-hero.com/api/peddy/pets")
+    const data = await res.json();
+    const petsData = data.pets;
+    petsData.forEach(petData => {
+        const card = document.createElement("div")
+        card.innerHTML = `
+        <div class="card bg-base-100 shadow-sm p-3 space-y-3">
+            <figure>
+                <img class=" w-96 h-52 rounded-md object-cover "
+                src=${petData.image}
+                alt="Pet" />
+            </figure>
+                <h3 class="text-xl font-bold">${petData.pet_name}</h3>
+                <div class="flex items-center primary-text gap-2 text-sm"><i class="fa-solid fa-anchor"></i><p>Breed :${petData.breed ? petData.breed : "not available"}<p></div>
+                <div class="flex items-center primary-text gap-2 text-sm"><i class="fa-solid fa-calendar-days"></i><p>Date :${petData.date_of_birth ? petData.date_of_birth : "not available"}<p></div>
+                <div class="flex items-center primary-text gap-2 text-sm"><i class="fa-solid fa-venus"></i><p>Gender :${petData.gender ? petData.gender : "not available"}<p></div>
+                <div class="flex items-center primary-text gap-2 text-sm"><i class="fa-solid fa-dollar-sign"></i><p>Price :${petData.price ? petData.price : "not available"}<p><i class="fa-solid fa-dollar-sign"></i></div>
+                
+                <div class="flex items-center justify-between">
+                <btn class="btn"><i class="fa-solid fa-thumbs-up"></i></btn>
+                <btn class="btn">Adopt</btn>
+                <btn class="btn">Details</btn>
+                </div>
+            
+            </div>
+        `
+        cardContainer.append(card)
+
+    });
+}
+displayAllCategories()
